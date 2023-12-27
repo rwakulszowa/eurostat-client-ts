132ba9ce9349b26a24af5de1dadffbe1f3af1a38
rwakulszowa <rwakulszowa1@gmail.com>
2023-12-27 23:28:49 +0100

    test: integrationt test with parse

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { CategoriesPerDimension, DatasetId, fetchDatasetRaw } from "./api-raw";
+import { Dataset, parseDatasetValues } from "./parse";
+export { fetchDatasetRaw } from "./api-raw";
+
+export async function fetchDataset(
+	datasetId: DatasetId,
+	categoriesPerDimension: CategoriesPerDimension,
+): Promise<Dataset> {
+	const raw = await fetchDatasetRaw(datasetId, categoriesPerDimension);
+	return parseDatasetValues(raw);
+}
--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,16 +1,35 @@
 import test from "ava";
 
-import { fetchDatasetRaw } from "../src/api-raw";
+import { fetchDataset, fetchDatasetRaw } from "../src/";
+
+const datasetId = "aact_ali01";
+const categoriesPerDimension = {
+	time: ["2000", "2010"],
+	geo: ["BE", "BG"],
+	freq: ["A"],
+	itm_newa: ["40000"],
+};
 
 test("fetchDatasetRaw", async (t) => {
-	const datasetId = "aact_ali01";
-	const categoriesPerDimension = {
-		time: ["2000", "2010"],
-		geo: ["BE", "BG"],
-		freq: ["A"],
-		itm_newa: ["40000"],
-	};
 	const result = await fetchDatasetRaw(datasetId, categoriesPerDimension);
 	t.is(result.class, "dataset");
 	t.deepEqual(Object.keys(result.value).length, 4);
+});
+
+test("fetchDataset", async (t) => {
+	const result = await fetchDataset(datasetId, categoriesPerDimension);
+	t.is(result.rows.length, 4);
+	t.deepEqual([...Object.keys(result.definitions)].sort(), [
+		"freq",
+		"geo",
+		"itm_newa",
+		"time",
+	]);
+	t.deepEqual(result.definitions.geo, {
+		label: "Geopolitical entity (reporting)",
+		categories: {
+			BE: { id: "BE", label: "Belgium" },
+			BG: { id: "BG", label: "Bulgaria" },
+		},
+	});
 });
